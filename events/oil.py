497b91a319bddd0f74e48b32d7f35548b3803875
shiun777 <139188661+shiun777@users.noreply.github.com>
2023-08-07 16:22:09 +0800

    Add files via upload

--- events/oil.py
+++ events/oil.py
@@ -10,5 +10,5 @@ def oil_price():
     title = soup.select('#main')[0].text.replace('\n', '').split('(')[0]
     gas_price = soup.select('#gas-price')[0].text.replace('\n\n\n', '').replace(' ', '')
     cpc = soup.select('#cpc')[0].text.replace(' ', '')
-    conten = '{}\n{}{}'.format(title, gas_price, cpc)
-    return conten
+    content = '{}\n{}{}'.format(title, gas_price, cpc)
+    return content
