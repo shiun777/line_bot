d19a0216b8dee408309b18d2f094f639f2f4e990
shiun777 <139188661+shiun777@users.noreply.github.com>
2023-08-07 15:00:09 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -26,5 +26,29 @@ def handle_message(event):
         about_us_event(event)
         Usage(event)
     
+    if event.message.text == "@小幫手":
+        buttons_template = TemplateSendMessage(
+            alt_text= '小幫手 template',
+            template=ButtonsTemplate(
+                title='請選擇',
+                thumbnail_image_url='https://imgur.com/gallery/t5gfibL',
+                actions=[
+                    MessageTemplateAction(
+                        label='油價查詢',
+                        text='油價查詢'
+                    ),
+                    MessageTemplateAction(
+                        label='匯率查詢',
+                        text='匯率查詢'
+                    ),
+                    MessageTemplateAction(
+                        label='股價查詢',
+                        text='股價查詢'
+                    )
+                ]
+            )
+        )
+        line_bot_api.reply_message(event.reply_token, buttons_template)
+    
 if __name__ == "__main__":
     app.run()
