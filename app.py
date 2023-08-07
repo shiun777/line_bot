a07aa46d85b78ca408e80c3c8823b35acbdc835a
shiun777 <139188661+shiun777@users.noreply.github.com>
2023-08-07 15:29:46 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -25,31 +25,8 @@ def handle_message(event):
     if messages_text == '@使用說明':
         about_us_event(event)
         Usage(event)
+     
     
-    if event.message.text == "@小幫手":
-        buttons_template = TemplateSendMessage(
-            alt_text= '小幫手 template',
-            template=ButtonsTemplate(
-                title='選擇服務',
-                text='請選擇',
-                thumbnail_image_url='https://i.imgur.com/ZgZ5lME.jpg',
-                actions=[
-                    MessageTemplateAction(
-                        label='油價查詢',
-                        text='油價查詢'
-                    ),
-                    MessageTemplateAction(
-                        label='匯率查詢',
-                        text='匯率查詢'
-                    ),
-                    MessageTemplateAction(
-                        label='股價查詢',
-                        text='股價查詢'
-                    )
-                ]
-            )
-        )
-        line_bot_api.reply_message(event.reply_token, buttons_template)
-    
+      
 if __name__ == "__main__":
     app.run()
--- events/basic.py
+++ events/basic.py
@@ -31,6 +31,31 @@ Hello!
         event.reply_token,
         [text_message, sticker_message]
     )
+    
+    buttons_template = TemplateSendMessage(
+            alt_text= '小幫手 template',
+            template=ButtonsTemplate(
+                title='選擇服務',
+                text='請選擇',
+                thumbnail_image_url='https://i.imgur.com/ZgZ5lME.jpg',
+                actions=[
+                    MessageTemplateAction(
+                        label='油價查詢',
+                        text='油價查詢'
+                    ),
+                    MessageTemplateAction(
+                        label='匯率查詢',
+                        text='匯率查詢'
+                    ),
+                    MessageTemplateAction(
+                        label='股價查詢',
+                        text='股價查詢'
+                    )
+                ]
+            )
+        )
+    line_bot_api.reply_message(event.reply_token, buttons_template)
+    
 def push_msg(event,msg):
     try:
         user_id = event.source.user_id
