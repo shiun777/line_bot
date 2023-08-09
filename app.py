2bdb1c547d361f7c651073f27518dd1d16e3779e
shiun777 <139188661+shiun777@users.noreply.github.com>
2023-08-09 16:38:53 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -62,9 +62,6 @@ def handle_message(event):
         line_bot_api.push_message(uid, TextSendMessage("加入股票代號"+stockNumber))
         content = write_my_stock(uid, user_name, stockNumber, msg[6:7], msg[7:])
         line_bot_api.push_message(uid, TextSendMessage(content))
-    else:
-        content = write_my_stock(uid, user_name, stockNumber, "未設定", "未設定")
-        line_bot_api.push_message(uid, TextSendMessage(content))
         return 0
     
     
