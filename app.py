51aa350a79535cfb8162b2f13d8741158274a18b
shiun777 <139188661+shiun777@users.noreply.github.com>
2023-08-11 18:47:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ def handle_message(event):
             url = "https://tw.stock.yahoo.com/q/q?s=" + stock
             list_req = requests.get(url)
             soup = BeautifulSoup(list_req.content, "html.parser")
-            getstock = soup.find("span", class_="Fz(32px) Fw(b) Lh(1) Mend(16px) D(f) Ai(c) C($c-trend-down)")
+            getstock = soup.find("div", class_="D(f) Ai(fe) Mb(4px)")
             content = stock + "當前股市價格為:" + getstock
             if condition == '<':
                 content += "\n篩選條件為: <" + price
