088fb2a4b68762707d4c193d187b408a3b87d385
shiun777 <139188661+shiun777@users.noreply.github.com>
2023-08-13 01:17:10 +0800

    Add files via upload

--- events/map.py
+++ events/map.py
@@ -3,8 +3,10 @@ from line_bot_api import *
 def goole_map(event):
     
     buttons_map = TemplateSendMessage(
-            alt_text= '小幫手 template',
+            alt_text= '附近資訊',
             template=ButtonsTemplate(
+                title='選擇服務',
+                text='請選擇',
                 actions=[
                     MessageTemplateAction(
                         label='附近停車場',
