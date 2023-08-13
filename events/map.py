66366e383d30e59f2f4c401b70075fb959a961ed
shiun777 <139188661+shiun777@users.noreply.github.com>
2023-08-13 19:04:37 +0800

    Add files via upload

--- events/map.py
+++ events/map.py
@@ -1,6 +1,7 @@
 from line_bot_api import *
-
-def goole_map(event):
+import requests
+import flask
+def google_map(event):
     
     buttons_map = TemplateSendMessage(
             alt_text= '附近資訊',
@@ -31,3 +32,51 @@ def goole_map(event):
         event.reply_token, 
         [buttons_map]
     )
+
+
+def get_user_location(user_id, api_key):
+    base_url = f"https://www.googleapis.com/geolocation/v1/geolocate?key={api_key}"
+    response = requests.post(base_url, json={})
+    data = response.json()
+
+    if "location" in data:
+        location_lat = data['location']['lat']
+        location_lng = data['location']['lng']
+        print(f"使用者 {user_id} 的地點：緯度 {location_lat}, 經度 {location_lng}")
+        return f"{location_lat},{location_lng}"
+
+    else:
+        print("無法獲取使用者地點。")
+        return None
+
+def search_nearby_parking(location, radius, api_key):
+    base_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+    params = {
+        "location": location,
+        "radius": radius,
+        "type": "parking",
+        "key": api_key
+    }
+
+    response = requests.get(base_url, params=params)
+    data = response.json()
+
+    if data['status'] == 'OK':
+        return data['results']
+    else:
+        print("搜尋附近停車場失敗。狀態:", data['status'])
+        return []
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    
+    return 'OK'
+
+
